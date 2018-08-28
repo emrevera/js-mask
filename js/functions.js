148c873ca6f070e4ad553a956e45c5d051f22b49
Ernesto Vera <emrevera@gmail.com>
2018-08-28 17:42:58 -0300

    Fixed bug so the mask wont get stuck on the borders

--- js/functions.js
+++ js/functions.js
@@ -3,22 +3,32 @@ document.addEventListener('DOMContentLoaded', function(){
 	const fixed = document.querySelector('.fixed');
 	const mask = document.querySelector('.mask');
 	const radious = mask.clientWidth / 2; 
-	console.log(mask);
 
 	function createMask(evt){
 
 		const posX = `${evt.clientX - radious}px`;
 		const posY = `${evt.clientY - radious}px`;
 
-		if (evt.clientX < radious || evt.clientY < radious) {
-			return;
-		} else {
+		if (evt.clientX < radious && evt.clientY < radious) {
+			mask.style.transform = `translate(0px, 0px)`;
+			fixed.style.transform = `translate(0px, 0px)`;
+		
+		}	else if (evt.clientY < radious){
+			mask.style.transform = `translate(${posX}, 0px)`;
+			fixed.style.transform = `translate(-${posX}, 0px)`;
+
+		}	else if (evt.clientX < radious) {
+			mask.style.transform = `translate(0px, ${posY})`;
+			fixed.style.transform = `translate(0px, -${posY})`;
+
+		}	else {
 			mask.style.transform = `translate(${posX}, ${posY})`;
-			mask.style.opacity = 1;
-			fixed.style.height = `${window.innerHeight}px`;
-			fixed.style.width = `${window.innerWidth}px`;
 			fixed.style.transform = `translate(-${posX}, -${posY})`;
 		}
+
+		fixed.style.height = `${window.innerHeight}px`;
+		fixed.style.width = `${window.innerWidth}px`;
+		mask.style.opacity = 1;
 	}
 
 	document.addEventListener('mousemove', createMask);
